de9243600213d5aff0dac8a21cee00f2bbdbd68a
Udey Rishi <udey.rishi@gmail.com>
2019-01-13 01:24:26 -0500

    Use lambda resolvers

--- src/main/schema.py
+++ src/main/schema.py
@@ -15,38 +15,39 @@ async def fetch_xml(url):
         raise ValueError(f'Received unexpected error code {response.status_code} from Goodreads API.')
 
 class Book(graphene.ObjectType):
-    title = graphene.NonNull(graphene.String, description="The title of the book.")
-    isbn = graphene.String(description="The ISBN of the book.")
-    isbn13 = graphene.String(description="The ISBN13 of the book.")
+    title = graphene.NonNull(
+        graphene.String, 
+        description="The title of the book.", 
+        resolver=lambda xml, info: xml.findall('title')[0].text
+    )
 
-    @staticmethod
-    def resolve_title(xml, info):
-        return xml.findall('title')[0].text
-
-    @staticmethod
-    def resolve_isbn(xml, info):
-        return xml.findall('isbn')[0].text
-
-    @staticmethod
-    def resolve_isbn13(xml, info):
-        return xml.findall('isbn')[0].text
+    isbn = graphene.String(
+        description="The ISBN of the book.", 
+        resolver=lambda xml, info: xml.findall('isbn')[0].text
+    )
+    
+    isbn13 = graphene.String(
+        description="The ISBN13 of the book.", 
+        resolver=lambda xml, info: xml.findall('isbn13')[0].text
+    )
 
 class Author(graphene.ObjectType):
-    id = graphene.NonNull(graphene.Int, description="The author's ID.")
-    name = graphene.NonNull(graphene.String, description="The name of the author.")
-    books = graphene.NonNull(graphene.List(Book))
+    id = graphene.NonNull(
+        graphene.Int, 
+        description="The author's ID.", 
+        resolver=lambda xml, info: int(xml.findall('id')[0].text)
+    )
     
-    @staticmethod
-    def resolve_id(xml, info):
-        return int(xml.findall('id')[0].text)
-
-    @staticmethod
-    def resolve_name(xml, info):
-        return xml.findall('name')[0].text
-
-    @staticmethod
-    def resolve_books(xml, info):
-        return xml.findall('books')[0].findall('book')
+    name = graphene.NonNull(
+        graphene.String, 
+        description="The name of the author.", 
+        resolver=lambda xml, info: xml.findall('name')[0].text
+    )
+    
+    books = graphene.NonNull(
+        graphene.List(Book), 
+        resolver=lambda xml, info: xml.findall('books')[0].findall('book')
+    )
 
 class Query(graphene.ObjectType):
     author = graphene.Field(Author, id=graphene.NonNull(graphene.Int), description='An author.')
