095b3418d4710817a5fd703dc3ae9eac69c0415d
Udey Rishi <udey.rishi@gmail.com>
2019-01-13 00:54:04 -0500

    Added books

--- src/main/schema.py
+++ src/main/schema.py
@@ -10,20 +10,42 @@ async def fetch_xml(url):
     file_ptr = urllib.request.urlopen(url)
     return xml.etree.ElementTree.parse(file_ptr).getroot()
 
+class Book(graphene.ObjectType):
+    title = graphene.NonNull(graphene.String, description="The title of the book.")
+    isbn = graphene.String(description="The ISBN of the book.")
+    isbn13 = graphene.String(description="The ISBN13 of the book.")
+
+    @staticmethod
+    def resolve_title(xml, info):
+        return xml.findall('title')[0].text
+
+    @staticmethod
+    def resolve_isbn(xml, info):
+        return xml.findall('isbn')[0].text
+
+    @staticmethod
+    def resolve_isbn13(xml, info):
+        return xml.findall('isbn')[0].text
+
 class Author(graphene.ObjectType):
-    id = graphene.Int(description="The author's ID.")
-    name = graphene.String(description="The name of the author.")
+    id = graphene.NonNull(graphene.Int, description="The author's ID.")
+    name = graphene.NonNull(graphene.String, description="The name of the author.")
+    books = graphene.NonNull(graphene.List(Book))
     
     @staticmethod
     def resolve_id(xml, info):
-        return xml.findall('id')[0].text
+        return int(xml.findall('id')[0].text)
 
     @staticmethod
     def resolve_name(xml, info):
         return xml.findall('name')[0].text
 
+    @staticmethod
+    def resolve_books(xml, info):
+        return xml.findall('books')[0].findall('book')
+
 class Query(graphene.ObjectType):
-    author = graphene.Field(Author, id=graphene.Int(), description='An author.')
+    author = graphene.Field(Author, id=graphene.NonNull(graphene.Int), description='An author.')
     
     @staticmethod
     async def resolve_author(_, info, id):
