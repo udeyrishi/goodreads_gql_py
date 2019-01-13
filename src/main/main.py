45c1ccd53bd459d2a3e3c465db5e28b365302325
Udey Rishi <udey.rishi@gmail.com>
2019-01-12 21:29:18 -0500

    Setup async resolvers

--- src/main/main.py
+++ src/main/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from graphql.execution.executors.asyncio import AsyncioExecutor
 from flask import Flask
 from flask_graphql import GraphQLView
 from schema import schema
@@ -7,5 +8,5 @@ from schema import schema
 if __name__ == '__main__':
     app = Flask(__name__)
     app.debug = True
-    app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
+    app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', executor=AsyncioExecutor(), schema=schema, graphiql=True))
     app.run()
--- src/main/schema.py
+++ src/main/schema.py
@@ -1,9 +1,12 @@
+import asyncio
 import graphene
+
 
 class Query(graphene.ObjectType):
     hello = graphene.String(argument=graphene.String(default_value="stranger"))
 
-    def resolve_hello(self, info, argument):
+    async def resolve_hello(self, info, argument):
+        await asyncio.sleep(5)
         return 'Hello ' + argument
 
 schema = graphene.Schema(query=Query)
