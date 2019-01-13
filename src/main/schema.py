91ac2e881eeaa5dd41c4a7f3e7fee15f0c0a4294
Udey Rishi <udey.rishi@gmail.com>
2019-01-13 00:36:57 -0500

    Make it more modular

--- src/main/schema.py
+++ src/main/schema.py
@@ -6,21 +6,28 @@ import graphene.relay as relay
 
 API_ROOT = 'https://www.goodreads.com'
 
-async def fetch_author(id, api_key):
-    file_ptr = urllib.request.urlopen(f"{API_ROOT}/author/show.xml?id={id}&key={api_key}")
-    author_xml = xml.etree.ElementTree.parse(file_ptr).getroot()
-    name = author_xml.findall('author')[0].findall('name')[0].text
-    return Author(id=id, name=name)
+async def fetch_xml(url):
+    file_ptr = urllib.request.urlopen(url)
+    return xml.etree.ElementTree.parse(file_ptr).getroot()
 
 class Author(graphene.ObjectType):
     id = graphene.Int(description="The author's ID.")
     name = graphene.String(description="The name of the author.")
+    
+    @staticmethod
+    def resolve_id(xml, info):
+        return xml.findall('id')[0].text
 
+    @staticmethod
+    def resolve_name(xml, info):
+        return xml.findall('name')[0].text
 
 class Query(graphene.ObjectType):
-    author = graphene.Field(Author, id=graphene.Int())
-
-    async def resolve_author(self, info, id):
-        return await fetch_author(id=id, api_key=info.context['api_key'])
+    author = graphene.Field(Author, id=graphene.Int(), description='An author.')
+    
+    @staticmethod
+    async def resolve_author(_, info, id):
+        xml = await fetch_xml(f"{API_ROOT}/author/show.xml?id={id}&key={info.context['api_key']}")
+        return xml.findall('author')[0]
 
 schema = graphene.Schema(query=Query)
