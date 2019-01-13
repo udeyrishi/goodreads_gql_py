a3dfb606d786b594f365967c551d224a2e7903ba
Udey Rishi <udey.rishi@gmail.com>
2019-01-12 19:51:03 -0500

    Added a simple graphql server

--- src/main/main.py
+++ src/main/main.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
 
+from flask import Flask
+from flask_graphql import GraphQLView
+from schema import schema
+
 if __name__ == '__main__':
-    print('hello, world!')
+    app = Flask(__name__)
+    app.debug = True
+    app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
+    app.run()
